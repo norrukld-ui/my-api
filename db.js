07a48ddb5efb4072ef3c3b34a4933b56d651e7b5
Мадина Бактыбек <madinabaktybek@MacBook-Air-Madina-2.local>
2026-04-10 22:03:16 +0500

    Connect to production database

--- db.js
+++ db.js
@@ -1,11 +1,14 @@
 const { Pool } = require('pg');
 
+// Render автоматически ставит NODE_ENV = 'production'
+const isProduction = process.env.NODE_ENV === 'production';
+
+// Если DATABASE_URL нет в окружении, используем твою новую ссылку
+const connectionString = process.env.DATABASE_URL || 'postgresql://my_auth_db_h1rq_user:p1FRSLhWPYUhAEWzOaMK3poEmcvklHgX@dpg-d783p8ffte5s738orq80-a/my_auth_db_h1rq';
+
 const pool = new Pool({
-  user: 'madinabaktybek',
-  host: 'localhost',
-  database: 'mini_instagram', // ✅ ВОТ ТУТ ИСПРАВЛЕНО
-  password: '',
-  port: 5432,
+  connectionString: connectionString,
+  ssl: isProduction ? { rejectUnauthorized: false } : false
 });
 
 module.exports = pool;
